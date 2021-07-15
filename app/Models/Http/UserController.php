295c71d681d6f8dbe7ee812b064a58c6f6a45dcf
new-item <32989514@qq.com>
2021-07-15 11:18:11 +0800

    first commit

---
+++ app/Models/Http/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Http;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserController extends Model
+{
+    use HasFactory;
+}
